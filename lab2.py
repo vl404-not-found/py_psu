26cafee44bb996def4cfd6ea899dbc5bc35e613a
vlad <shkerdin.v@ya.ru>
2020-11-01 22:31:58 +0300

    add search funcs

--- lab2.py
+++ lab2.py
@@ -1,11 +1,49 @@
-def is_equal_str(str_x, str_y):
-    i = 0
-    while i <= len(str_x) and str_x[i] == str_y[i]:
-        i += 1
+from loremipsum import get_sentences
+import random
+
+
+def iter_func(k):
+    global iter_operations
+    iter_operations += k
+
+
+# zzz_ - is a prefix
+def zzz_is_equal_str(str_x: str, str_y: str):
+    iter_func(1)
+    counter = 0
+    while (counter < len(str_y) or counter < len(str_x)) and str_x[counter] == str_y[counter]:
+        iter_func(9)
+        counter += 1
+    iter_func(2)
+    return counter == len(str_x)
+
+
+def zzz_search_in_list(search_item: str, _list: list):
+    iter_func(1)
+    check = False
+    for iteration in range(0, len(_list)):
+        iter_func(1)
+        if zzz_is_equal_str(_list[iteration], search_item):
+            print(f'\n {search_item} find at: {iteration}')
+            check = True
+    if not check:
+        iter_func(1)
+        raise BaseException("Not found")
 
 
 if __name__ == '__main__':
-    try:
-        pass
-    except BaseException as e:
-        print(e)
+    iter_operations = 0
+    # Взято из библиотеки генерации рандомных текстов
+    _list = get_sentences(50000)
+
+    # искомая строка
+    # Случайно задаётся их всего массива строк
+    search = _list[random.randint(0, len(_list) - 1)]
+
+    for i in range(100):
+        try:
+            iter_func(-iter_operations)
+            zzz_search_in_list(search, _list)
+        except BaseException as e:
+            print(e)
+    print(iter_operations)
