7137fb415269067245c96e0c1ee9e985763c26d3
vlad <shkerdin.v@ya.ru>
2020-11-23 09:07:44 +0300

    add search funcs

--- lab2.py
+++ lab2.py
@@ -11,7 +11,8 @@ def iter_func(k):
 def zzz_is_equal_str(str_x: str, str_y: str):
     iter_func(1)
     counter = 0
-    while (counter < len(str_y) or counter < len(str_x)) and str_x[counter] == str_y[counter]:
+    while (counter < len(str_y) or counter < len(str_x)) \
+            and str_x[counter] == str_y[counter]:
         iter_func(9)
         counter += 1
     iter_func(2)
@@ -31,6 +32,22 @@ def zzz_search_in_list(search_item: str, _list: list):
         raise BaseException("Not found")
 
 
+def zzz_inline_search(_list, str_y):
+    iter_inline = 0
+    _list1 = _list
+    _list1.append(str_y)
+    while True:
+        iter_inline += 1
+        j = 0
+        while j < len(str_y) and _list1[iter_inline].split()[j]:
+            j += 1
+        if j > len(str_y):
+            if iter_inline >= len(_list):
+                raise BaseException('Not found')
+            else:
+                print(f'\n {str_y} find at: {iter_inline}')
+
+
 if __name__ == '__main__':
     iter_operations = 0
     # Взято из библиотеки генерации рандомных текстов
@@ -43,7 +60,7 @@ if __name__ == '__main__':
     for i in range(100):
         try:
             iter_func(-iter_operations)
-            zzz_search_in_list(search, _list)
+            zzz_inline_search(search, _list)
         except BaseException as e:
             print(e)
     print(iter_operations)
