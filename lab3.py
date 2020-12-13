3eb0b3c86f83e1bc9c493b4637fdef7130020a87
vlad <shkerdin.v@ya.ru>
2020-12-14 00:38:33 +0300

    refactor and add search funcs

--- lab3.py
+++ lab3.py
@@ -1,6 +1,15 @@
 # help funcs
 import math
-# import random
+import random
+
+
+def create_ex_data():
+    with open('example.txt', 'w') as file:
+        for it in range(10000):
+            if it < 1:
+                file.write(str(random.randint(0, 3200)))
+            else:
+                file.write("\n" + str(random.randint(0, 3200)))
 
 
 def convert_base(num, to_base=10, from_base=10):
@@ -37,19 +46,41 @@ class HashArray:
         if flag:
             self._dict.update({key_a: [value]})
 
+    def searching(self, key_a, value_a):
+        for key in self._dict.keys():
+            if key == key_a:
+                for value in self._dict.get(key):
+                    if value == value_a:
+                        return key
+        return -1
+
     def add_num_divider(self, num):
         conv = int(num // self.divider) + 1
         self.adding(conv, num)
 
+    def search_divider(self, num):
+        conv = int(num // self.divider) + 1
+        self.searching(conv, num)
+
     def add_num_sys(self, num):
         conv = int(str(convert_base(num, self.to_num_sys))[:self.size])
         self.adding(conv, num)
+
+    def search_num_sys(self, num):
+        conv = int(str(convert_base(num, self.to_num_sys))[:self.size])
+        self.searching(conv, num)
 
     def add_num_square(self, num):
         conv = str(num)[
                int(int(_len(num) / 2) - self.size):
                int(_len(num) / 2) + math.ceil(self.size)]
         self.adding(conv, num)
+
+    def search_num_square(self, num):
+        conv = str(num)[
+               int(int(_len(num) / 2) - self.size):
+               int(_len(num) / 2) + math.ceil(self.size)]
+        self.searching(conv, num)
 
     def add_coagulation(self, num):
         pre = 0
@@ -62,6 +93,17 @@ class HashArray:
                 dec_counter = 0
 
         self.adding(int(str(pre)[-self.size:]), num)
+
+    def search_coagulation(self, num):
+        pre = 0
+        dec_counter = 0
+        for a_iter in str(num):
+            if dec_counter != self.size:
+                dec_counter += 1
+                pre += int(a_iter) * 10 ** dec_counter
+            else:
+                dec_counter = 0
+        self.searching(pre, num)
 
 
 if __name__ == '__main__':
@@ -89,10 +131,3 @@ if __name__ == '__main__':
         for i in f.readlines():
             array.add_num_divider(int(i))
         f.close()
-
-# with open('example.txt', 'w') as f:
-#     for i in range(10000):
-#         if i < 1:
-#             f.write(str(random.randint(0, 3200)))
-#         else:
-#             f.write("\n" + str(random.randint(0, 3200)))
